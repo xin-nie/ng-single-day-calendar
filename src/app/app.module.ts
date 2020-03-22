636b45423a5aa59747d60e309b7ec263ace39673
Xin Nie <xnie@groupon.com>
2020-03-22 22:59:09 +0100

    feat: add core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { LayoutComponent } from './layout/layout.component';
+
+@NgModule({
+  declarations: [LayoutComponent],
+  imports: [
+    BrowserModule,
+  ],
+  exports: [LayoutComponent],
+})
+export class CoreModule { }
